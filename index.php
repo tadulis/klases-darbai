ff93f1afe8b308a977c9e8ebcebd2980ab7e6162
Tadas <t..petruskevicius@gmail.com>
2020-02-03 10:46:18 +0200

    pirma uzduotis

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
 
+// "1. ekrane atvazduokite teksta 'Šios dienos data - ' ir su php atvaizuokite šios dienos datą"
+
+$string = 'Šios dienos data - ';
+$date = date('Y/m/d');
+
+$result = $string . $date;
+
+print $result;
