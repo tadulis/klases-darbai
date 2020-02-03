3d3d86bcb5e7f0f14134e378742f2fd01176143e
Tadas <t..petruskevicius@gmail.com>
2020-02-03 16:17:54 +0200

    flower cards from array

--- index.php
+++ index.php
@@ -1,23 +1,39 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// "Sukurkite FOR cikla kuris prasidetu nuo 0, veiktu iki kol kintamasisi pataps 10, ir atspausdins savo ciklo reiksme kas 1 vieneta"
-
-for($x=0;$x<=10;$x++){
-	print $x;
-}
-
-print '<br>';
-
-for($x=0;$x<=10;$x+=2){
-	print $x;
-}
-
-print '<br>';
-
-for($x=0;$x<=10;$x+=5){
-	print $x;
-}
+	$cards = 
+	[
+		[
+			'url' => 'https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+			'alt' => 'roze',
+			'text' => 'raudona roze',
+		],
+		[
+			'url' => 'https://upload.wikimedia.org/wikipedia/commons/b/ba/Flower_jtca001.jpg',
+			'alt' => 'kazkokia',
+			'text' => 'kazkokia gele',
+		],
+		[
+			'url' => 'https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+			'alt' => 'kazkokia1',
+			'text' => 'kazkokia1 gele',
+		],
+	];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		/*aprasyti korteliu stiliu */
+	</style>
+</head>
+<body>
+	<section class="cards">
+		<?php foreach ($cards as $value):?>
+		<div class="card">
+			<img src="<?php print $value['url']; ?>" alt="<?php print $value['alt']; ?>">
+			<p><?php print $value['text']; ?></p>
+		</div>
+		<?php endforeach; ?>
+	</section>
+</body>
+</html>
